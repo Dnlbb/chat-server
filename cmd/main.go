69930faae69715f1dd99a4bb77c5050b77ccc217
Daniil Bykov <daniil0604bykiv@gmail.com>
2024-09-10 09:06:13 +0300

    реализована холостая логика и исправлены ошибки чтобы не ругался линтер

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,50 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"net"
 
-	"github.com/fatih/color"
+	desc "github.com/Dnlbb/chat-server/pkg/chat"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
 )
 
+type server struct {
+	desc.UnimplementedShatServer
+}
+
+func (s *server) Create(_ context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+	log.Printf("Users: %v", req.GetUsernames())
+	return &desc.CreateResponse{}, nil
+}
+
+func (s *server) Delete(_ context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
+	log.Printf("Id: %d", req.GetId())
+	return nil, nil
+}
+
+func (s *server) SendMessage(_ context.Context, req *desc.SendMessageRequest) (*emptypb.Empty, error) {
+	log.Printf("From: %s", req.GetFrom())
+	log.Printf("Text: %s", req.GetText())
+	log.Printf("Time: %v", req.GetTimestamp())
+	return nil, nil
+}
+
 func main() {
-	fmt.Println(color.GreenString("Hello, world!"))
+	lis, err := net.Listen("tcp", "127.0.0.1:50050")
+	if err != nil {
+		log.Fatal("failed to listen: 50050 ")
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+	desc.RegisterShatServer(s, &server{})
+
+	log.Printf("server listening at #{lis.Addr()}")
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatal("failed to serve: #{err}")
+	}
 }
